a1228570dee6dc10c1d913551f27a60427fa4dbf
Anton Danilov <hello@deviun.me>
2025-07-23 18:55:51 +0300

    fix: docker entrypoint

--- scripts/docker/docker-entrypoint.sh
+++ scripts/docker/docker-entrypoint.sh
@@ -27,16 +27,15 @@ function checkAndStartDocker() {
 if [[ $FARM_DEPLOYMENT_MODE == "vm" ]]; then
 echo "Starting docker..."
 checkAndStartDocker
-fi
 
 if [[ -z "$DOCKER_LOGIN" || -z "$DOCKER_PWD" ]]; then
     echo "Warning: DOCKER_LOGIN or DOCKER_PWD not defined, skipping docker login"
 else
     echo $DOCKER_PWD | docker login --username $DOCKER_LOGIN --password-stdin $DOCKER_REGISTRY
-    if [[ $FARM_DEPLOYMENT_MODE == "vm" ]]; then
     ## on restart container docker service may down after starting by unknown reason, check and try again
     checkAndStartDocker
-    fi
+fi
+
 fi
 
 ## prepare docker network
