7c2d9e30ed7da754c9a6cd15d7d4016db9b826ac
Mikhail Golbakh <golbah@yandex.ru>
2026-02-02 15:15:26 +0100

    feat: support multi-host for postgres

--- src/server/utils/db/knexfile.ts
+++ src/server/utils/db/knexfile.ts
@@ -21,15 +21,30 @@ const getCommonSettings = (): Knex.Config => {
     };
 
     if (DB_TYPE === 'postgres') {
+        const Client_PG: typeof Knex.Client = require('knex/lib/dialects/postgres');
+
+        class CustomClient_PG extends Client_PG {
+            async validateConnection(connection: any): Promise<boolean> {
+                try {
+                    const [row] = await connection.query('SELECT pg_is_in_recovery()');
+                    return row?.pg_is_in_recovery === false;
+                } catch (error) {
+                    console.error('Connection validation failed', error);
+                    return false;
+                }
+            }
+        }
+
         return {
             ...baseSettings,
-            client: 'pg',
+            client: CustomClient_PG,
             connection: {
                 host: process.env.FARM_DB_HOST || 'localhost',
                 port: parseInt(process.env.FARM_DB_PORT || '5432', 10),
                 user: process.env.FARM_DB_USER || 'postgres',
                 password: process.env.FARM_DB_PASSWORD || 'postgres',
                 database: process.env.FARM_DB_NAME || 'farm',
+                options: '-c target_session_attrs=read-write',
             },
         };
     }
