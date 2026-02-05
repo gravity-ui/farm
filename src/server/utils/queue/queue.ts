cd6c6baa7731c71e02f126d4db76a41677b9145d
Oleg <31084097+JablDouble@users.noreply.github.com>
2026-02-05 15:59:42 +0300

    feat(rebuild-instance): remove unused code for rebuild instances (#32)

--- src/server/utils/queue/queue.ts
+++ src/server/utils/queue/queue.ts
@@ -3,24 +3,17 @@ import * as db from '../../utils/db';
 import {wrapInternalError} from '../common';
 
 import {BUILDS_LIMIT} from './constants';
-import {
-    deleteInstance,
-    generateInstance,
-    getInstancesToRestart,
-    restartFailedBuilds,
-} from './utils';
+import {deleteInstance, generateInstance} from './utils';
 
 export class Queue {
     declare private locker: {
         deleteInstances: boolean;
-        restartFailedBuilds: boolean;
         buildInstances: boolean;
     };
 
     constructor() {
         this.locker = {
             deleteInstances: false,
-            restartFailedBuilds: false,
             buildInstances: false,
         };
     }
@@ -43,27 +36,6 @@ export class Queue {
             );
         } finally {
             this.locker.deleteInstances = false;
-        }
-    };
-
-    restartFailedBuilds = async () => {
-        if (this.locker.restartFailedBuilds) {
-            return;
-        }
-
-        try {
-            this.locker.restartFailedBuilds = true;
-            const instancesToRestart = await getInstancesToRestart();
-
-            if (instancesToRestart.length > 0) {
-                restartFailedBuilds(instancesToRestart).catch((e) => {
-                    getNodeKit().ctx.logError('QUEUE::restartFailedBuilds', wrapInternalError(e));
-                });
-            }
-        } catch (e) {
-            getNodeKit().ctx.logError('QUEUE::restartFailedBuilds', wrapInternalError(e));
-        } finally {
-            this.locker.restartFailedBuilds = false;
         }
     };
 
